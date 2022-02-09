1d3a23381634bd674f946d011080b538d2db8111
jinyunjae <jinyun3075@naver.com>
2022-02-09 13:03:18 +0900

    DB 설정

--- GamgulServer/model/config.js
+++ GamgulServer/model/config.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const setDB = async ()=>{
-    mongoose.connect(process.env.DATA_URL);
-    // mongoose.connect(process.env.LOCAL);
+    // mongoose.connect(process.env.DATA_URL);
+    mongoose.connect(process.env.LOCAL);
     const db = mongoose.connection;
     db.on('error',()=>{
         console.log("DB연결 실패");
