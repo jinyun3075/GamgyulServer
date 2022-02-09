0d9ffca82dbae044c96eb963656bd37af12458c0
jinyunjae <jinyun3075@naver.com>
2022-02-09 13:04:29 +0900

    Local DB

--- GamgulServer/model/config.js
+++ GamgulServer/model/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const setDB = async ()=>{
     // mongoose.connect(process.env.DATA_URL);
-    mongoose.connect(process.env.LOCAL);
+    mongoose.connect("mongodb://localhost:27017/gamgyul");
     const db = mongoose.connection;
     db.on('error',()=>{
         console.log("DB연결 실패");
