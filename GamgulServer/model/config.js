4f25cc1b52fa01a3afb23e80980266a541691b87
jinyunjae <jinyun3075@naver.com>
2022-02-17 21:43:16 +0900

    ADD Dockerfile

--- GamgulServer/model/config.js
+++ GamgulServer/model/config.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const setDB = async ()=>{
-    mongoose.connect(process.env.DATA_URL);
-    // mongoose.connect("mongodb://localhost:27017/gamgyul");
+    // mongoose.connect(process.env.DATA_URL);
+    mongoose.connect("mongodb://localhost:27017/gamgyul");
     const db = mongoose.connection;
     db.on('error',()=>{
         console.log("DB연결 실패");
